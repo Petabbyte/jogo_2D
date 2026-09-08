050eefdda5ab6ef3bdce212a5a2e2392c5d94f7c
mavimangos67 <vianavictor177@gmail.com>
2026-09-08 12:11:37 -0300

    adicionei a movimentaçao do player
    fiz um script para a movimentaçao e arrumei os bugs do sprit

---
+++ Assets/scipts/movimentoi lateral do player.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class movimentoilateraldoplayer : MonoBehaviour
+{
+    public float speed = 5f;
+    public float horizontal;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        transform.position += Vector3.right * horizontal * speed * Time.deltaTime;
+    }
+}
