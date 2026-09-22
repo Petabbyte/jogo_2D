c1eb028ef0cd4ba9b6205b5c6c00449abe54dcf2
mavimangos67 <vianavictor177@gmail.com>
2026-09-22 09:31:47 -0300

    Script Player
    Atualizei o codigo do player agora ele so pula quando estiver encostando no chão
    e arrumei os componentes do "player" e do "Ground".

--- Assets/scipts/Player.cs
+++ Assets/scipts/Player.cs
@@ -4,6 +4,8 @@ public class Player : MonoBehaviour
 {
     public float speed = 5f;
     private Rigidbody2D rb;
+
+    private bool isGrounded = false;
     
     void Start()
     {
@@ -17,9 +19,25 @@ public class Player : MonoBehaviour
 
         rb.linearVelocity = new Vector2(moveHorizontal * speed, rb.linearVelocity.y); // vai aplicar a velocidade horizontal 
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) // com o "isgrounded" na condição do if agora o player so vai pular quando a tecla space tiver precionada e quando o pleyer estiver no chao
         {
-            rb.AddForce(new Vector2(0f ,5f), ForceMode2D.Impulse); //
+            rb.AddForce(new Vector2(0f ,5f), ForceMode2D.Impulse); 
+        }
+    }
+
+    void OnCollisionEnter2D( Collision2D collision) 
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true; //vai reconhecer quando o jogador estiver encostando no chao
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false; //vai reconhecer quando o jogador deixar de encostar no chao
         }
     }
 }
